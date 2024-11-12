6e2092a54333ca771ca6e68333fcb607cf416a74
Dylan Penney <penneydylan@icloud.com>
2024-11-12 17:55:57 +0000

    Time: 4 ms (64.29%), Space: 17.9 MB (64.77%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,14 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        l = 0
+
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[r], nums[l] = nums[l], nums[r]
+                l += 1
