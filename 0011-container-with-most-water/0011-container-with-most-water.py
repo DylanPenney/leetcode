c03d35de12ab510f8b2688a6763828e3d3a36347
Dylan Penney <penneydylan@icloud.com>
2024-11-12 13:38:38 +0000

    Time: 99 ms (59.54%), Space: 27.6 MB (89.04%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,20 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+        max_water = 0
+        while l <= r:
+
+            # Area of water between pole at index r and pole at index l
+            max_water = max(max_water, (r-l) * min(height[r], height[l]))
+
+            # Then we move the smaller pole towards the other
+            if height[r] > height[l]:
+                l += 1
+            else:
+                r -= 1
+
+        return max_water
