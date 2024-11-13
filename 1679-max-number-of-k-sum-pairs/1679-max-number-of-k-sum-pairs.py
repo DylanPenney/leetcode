b1840bfab7556e53f2180d8e5c6d6b87a4a3f516
Dylan Penney <penneydylan@icloud.com>
2024-11-13 19:33:23 +0000

    Time: 502 ms (37.9%), Space: 29.6 MB (37.51%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -1,22 +1,23 @@
-# Time Complexity: O(nlogn)
-# Space Complexity: O(1)
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        l = 0
-        r = len(nums) - 1
+        hash_map = {}
 
         res = 0
 
-        nums.sort()
-
-        while l < r:
-            lr_sum = nums[l] + nums[r]
-            if lr_sum == k:
+        for n in nums:
+            if n in hash_map:
+                # Pair found, decrement counter and add to result
+                hash_map[n] -= 1
                 res += 1
-                l += 1
-                r -= 1
-            elif lr_sum > k:
-                r -= 1
+
+                # remove from hashmap so future occurances don't get messed up
+                if hash_map[n] == 0:
+                    del hash_map[n]
+
             else:
-                l += 1
+                # Number needed and the amount needed
+                hash_map[k-n] = hash_map.get(k-n, 0) + 1
+
         return res
