e64502afcb46a27ded568ad358e237bad22a616b
Dylan Penney <penneydylan@icloud.com>
2024-11-14 17:38:38 +0000

    Time: 0 ms (100%), Space: 16.6 MB (75.87%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -8,6 +8,8 @@ class Solution:
         while l < r:
             m = l + (r-l)//2
 
+            # as is ascending and should all be increments of 1, 
+            # we can work out the amount of missing numbers l of m
             missing_nums = arr[m] - m - 1
 
             if k <= missing_nums:
