cf1811fc7db4c40b6a47a4e51dbc0108c1ed0c96
Dylan Penney <penneydylan@icloud.com>
2024-11-15 14:42:30 +0000

    Time: 0 ms (100%), Space: 16.9 MB (17.5%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -1,3 +1,5 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         max_candies = max(candies)
