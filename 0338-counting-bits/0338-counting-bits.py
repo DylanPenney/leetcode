c9c9f5bb140eaf83111f70e7aaade092990eba18
Dylan Penney <penneydylan@icloud.com>
2024-11-18 11:08:23 +0000

    Time: 3 ms (95%), Space: 17.7 MB (56.12%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,13 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n+1)
+
+        for i in range(1, n+1):
+            if i % 2 == 0:
+                ans[i] = ans[i//2]
+            else:
+                ans[i] = ans[i-1]+1
+
+        return ans
