ad7b49fd79f178da5ec957dfe3f5f9af0394fc07
Dylan Penney <penneydylan@icloud.com>
2024-11-19 13:58:25 +0000

    Time: 3 ms (94.48%), Space: 17.9 MB (12.18%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,9 +1,11 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
 
         for astrd in asteroids:
-            while stack and astrd < 0:
+            while stack and astrd < 0 < stack[-1]:
                 if stack[-1] < -1 * astrd:
                     # right-moving asteroid explodes, move onto next
                     stack.pop()
