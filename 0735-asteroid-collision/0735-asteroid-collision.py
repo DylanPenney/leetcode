ae22e1137b64afc7f9a6052049fe2b01ccc3a475
Dylan Penney <penneydylan@icloud.com>
2024-11-19 13:58:10 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,23 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+
+        for astrd in asteroids:
+            while stack and astrd < 0:
+                if stack[-1] < -1 * astrd:
+                    # right-moving asteroid explodes, move onto next
+                    stack.pop()
+                    continue
+                
+                elif stack[-1] == -1 * astrd:
+                    # both asteroids explode
+                    stack.pop()
+                    break
+
+                # right-moving asteroid destroys left-moving, move to next asteroid in paramter
+                break
+            else:
+                stack.append(astrd)
+
+        return stack
+            
