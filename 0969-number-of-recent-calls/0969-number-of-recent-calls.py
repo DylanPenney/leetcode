fab0402f323b9eff1b24f74587c7a40a2b80aef3
Dylan Penney <penneydylan@icloud.com>
2024-11-20 17:18:25 +0000

    Time: 124 ms (18.04%), Space: 22.6 MB (13.43%) - LeetHub

---
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -0,0 +1,27 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.queue = []
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+
+        cutoff = t - 3000
+        l = 0
+        r = len(self.queue) - 1
+
+        while (l < r):
+            m = l + (r-l)//2
+
+            if self.queue[m] < cutoff:
+                l = m + 1
+            else:
+                r = m
+        
+        self.queue = self.queue[r:]
+
+        return len(self.queue)
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
